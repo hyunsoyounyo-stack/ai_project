1ba7327ee47243f4e5919ae461d66fd85478f1a3
hyunsoyounyo-stack <hyunsoyounyo@gmail.com>
2026-05-18 10:23:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ st.title('나의 첫 웹 서비스 만들기')
 a=st.text_input('이름을 입력해주세요')
 b=st.selectbox('좋아하는 음식을 선택하세요',['케이크','쿠키','마늘빵'])
 if st.button('인삿말 생성'):
-  st.write(a+'님, 안녕하세요')\\
+  st.write(a+'님, 안녕하세요')
   st.info('반갑습니다')
   st.warning(b+'간식을 좋아하시나봐요!')
   st.error('잘 부탁합니다')
